7f552da6fd745184ee3d86b51c0032f03f32e84a
U-Beanolith\ninji <Jason Viers>
2015-08-22 16:01:23 -0400

    Set up basic cat with animation controller

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Cat : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody2D rb;
+    private Transform tSprite;
+
+    public void Start() {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        tSprite = transform.Find("sprite");
+    }
+
+    public void Update() {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+        anim.SetFloat("hSpeed", Mathf.Abs(rb.velocity.x));
+        // flip the sprite if necessary
+        if ((rb.velocity.x > 0 && tSprite.localScale.x < 0f)
+                || (rb.velocity.x < 0 && tSprite.localScale.x > 0f)) {
+            tSprite.localScale = new Vector3(
+                -tSprite.localScale.x,
+                tSprite.localScale.y,
+                tSprite.localScale.z);
+            }
+    }
+    
+}
